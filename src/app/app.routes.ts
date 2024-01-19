69ccb8caabe546be54a5e78b7ffe1267e0fba3ea
dinesh6369@ <dineshkumar636905@gmail.com>
2024-01-19 21:08:32 +0530

    crm -practice

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,44 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MyProductsComponent } from './my-products/my-products.component';
+import { Component } from '@angular/core';
+import { AddProductComponent } from './add-product/add-product.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'login',
+        component: LoginComponent,
+    },
+    {
+        path: 'dashboard',
+        component: DashboardComponent,
+    },
+    {
+        path: 'myproducts',
+        component: MyProductsComponent,
+        children: [
+            {
+                path: 'add-product',
+                component: AddProductComponent,
+            },
+            {
+                path: 'product-details',
+                component: ProductDetailsComponent,
+            }
+        ]
+    },
+    // {
+    //     path: 'add-product',
+    //     component: AddProductComponent,
+    // },
+    // {
+    //     path: 'product-details',
+    //     component: ProductDetailsComponent,
+    // },
+    {
+        path: '**',
+        component: LoginComponent,
+    },
+];
