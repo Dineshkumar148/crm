8a3f44e57686931cd5c23a6c978f4cfe257d282d
dinesh6369@ <dineshkumar636905@gmail.com>
2024-01-22 07:41:05 +0530

    routing active color change

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -39,6 +39,6 @@ export const routes: Routes = [
     // },
     {
         path: '**',
-        component: LoginComponent,
+        redirectTo: '/login',
     },
 ];
--- src/app/my-products/my-products.component.ts
+++ src/app/my-products/my-products.component.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-my-products',
   standalone: true,
-  imports: [CommonModule,RouterOutlet,RouterLink],
+  imports: [CommonModule,RouterOutlet,RouterLink,RouterLinkActive],
   templateUrl: './my-products.component.html',
   styleUrl: './my-products.component.css'
 })
